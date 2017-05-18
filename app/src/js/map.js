ab07b7d3e58df81c297b347eff0ed96b1df7f936
Julian Dietz <julian.dietz@stud.uni-regensburg.de>
2017-05-18 17:32:39 +0200

    Fix git

--- app/src/js/map.js
+++ app/src/js/map.js
@@ -88,8 +88,6 @@ d3.map = function (data) {
     function ChoroplethColor(curyear) {
         var transform = data.getMapData(curyear);
         clearMapColor();
-<<<<<<< Updated upstream
-=======
 
         //http://stackoverflow.com/questions/14167863/how-can-i-bring-a-circle-to-the-front-with-d3
         d3.selection.prototype.moveToFront = function () {
@@ -107,8 +105,6 @@ d3.map = function (data) {
             });
         };
 
-
->>>>>>> Stashed changes
         var tooltip = d3.select("body")
             .append("div")
             .style("position", "absolute")
